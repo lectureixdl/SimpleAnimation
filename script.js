874df3914c923e71bef2357f94d83010f75dba81
lectureixdl <lecture@ixdl.kr>
2014-09-11 22:35:25 +0900

    Add sprite animation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
+var cow;
 
 function preload() {
-	game.load.image('cow', 'assets/cow.png');
+	game.load.spritesheet('cow', 'assets/cow.png',100 , 50);
 }
 
 function create() {
-	game.add.sprite(100, 100, 'cow');
+	cow = game.add.sprite(100, 100, 'cow');
+	cow.animations.add('walk');
+	cow.animations.play('walk', 5, true);
 }
